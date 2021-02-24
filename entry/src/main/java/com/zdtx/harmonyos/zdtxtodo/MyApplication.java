23a4ba062686845474a46ed0368c5fb6b38703d7
郭陈 <guochenhome@163.com>
2021-02-24 16:58:44 +0800

    日志管理

--- entry/src/main/java/com/zdtx/harmonyos/zdtxtodo/MyApplication.java
+++ entry/src/main/java/com/zdtx/harmonyos/zdtxtodo/MyApplication.java
@@ -1,10 +1,12 @@
 package com.zdtx.harmonyos.zdtxtodo;
 
+import com.zdtx.harmonyos.zdtxtodo.kits.ZLog;
 import ohos.aafwk.ability.AbilityPackage;
 
 public class MyApplication extends AbilityPackage {
     @Override
     public void onInitialize() {
         super.onInitialize();
+        ZLog.init(BuildConfig.DEBUG, "指动天下");
     }
 }
--- entry/src/main/java/com/zdtx/harmonyos/zdtxtodo/slice/MainAbilitySlice.java
+++ entry/src/main/java/com/zdtx/harmonyos/zdtxtodo/slice/MainAbilitySlice.java
@@ -1,13 +1,20 @@
 package com.zdtx.harmonyos.zdtxtodo.slice;
 
 import com.zdtx.harmonyos.zdtxtodo.ResourceTable;
+import com.zdtx.harmonyos.zdtxtodo.kits.ZLog;
 import ohos.aafwk.ability.AbilitySlice;
 import ohos.aafwk.content.Intent;
 import ohos.agp.components.Button;
 import ohos.agp.components.Component;
+import ohos.hiviewdfx.HiLog;
+import ohos.hiviewdfx.HiLogLabel;
 
 public class MainAbilitySlice extends AbilitySlice {
     private Button button;
+    private Button button_log;
+
+    private static final HiLogLabel LABEL = new HiLogLabel(HiLog.LOG_APP, 0x00201, "MY_TAG");
+
     @Override
     public void onStart(Intent intent) {
         super.onStart(intent);
@@ -17,12 +24,20 @@ public class MainAbilitySlice extends AbilitySlice {
 
     private void initView(){
         button=(Button) findComponentById(ResourceTable.Id_btn_to_info);
+        button_log=(Button)findComponentById(ResourceTable.Id_btn_log);
         button.setClickedListener(new Component.ClickedListener() {
             @Override
             public void onClick(Component component) {
                 present(new MainInfoAbilitySlice(),new Intent());
             }
         });
+        button_log.setClickedListener(new Component.ClickedListener() {
+            @Override
+            public void onClick(Component component) {
+                ZLog.i("TAG","Harmony你好！");
+            }
+        });
+
     }
 
     @Override
